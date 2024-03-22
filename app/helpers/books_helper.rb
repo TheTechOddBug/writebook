5f141ed23132ba86e0aa3b045fc7a2df8edbea16
Jason Zimdars <107673+jzimdars@users.noreply.github.com>
2024-03-22 17:01:56 -0500

    Basic layout for page navigation

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,13 +1,22 @@
 module BooksHelper
   def link_to_previous_leafable(leaf)
     if previous_leaf = leaf.previous
-      link_to "≪ " + previous_leaf.title, leafable_path(previous_leaf), data: { **hotkey_data_attributes("left") }
+      link_to leafable_path(previous_leaf), data: { **hotkey_data_attributes("left") }, class: "flex align-center gap full-width flex-item-grow min-width justify-start" do
+        tag.span(class: "btn") do
+          image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span("Previous", class: "for-screen-reader")
+        end + tag.span(previous_leaf.title, class: "overflow-ellipsis")
+      end
     end
   end
 
   def link_to_next_leafable(leaf)
     if next_leaf = leaf.next
-      link_to next_leaf.title + " ≫", leafable_path(next_leaf), data: { **hotkey_data_attributes("right") }
+      link_to leafable_path(next_leaf), data: { **hotkey_data_attributes("right") }, class: "flex align-center gap full-width flex-item-grow min-width justify-end" do
+        tag.span(next_leaf.title, class: "overflow-ellipsis") +
+        tag.span(class: "btn") do
+          image_tag("arrow-right.svg", aria: { hidden: true }, size: 24) + tag.span("Next", class: "for-screen-reader")
+        end
+      end
     end
   end
 
