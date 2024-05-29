0dcc4da7b0b1973438204d13c5f5731b9f2b21db
Alberto Fernández-Capel <afcapel@gmail.com>
2024-05-29 13:42:23 +0100

    Extract helper methods

--- app/models/leaf/editable.rb
+++ app/models/leaf/editable.rb
@@ -8,23 +8,29 @@ module Leaf::Editable
   end
 
   def edit(leafable_params: {}, leaf_params: {})
-    transaction do
-      changes_to_leafable = leafable_params.select do |key, value|
-        leafable.attributes[key.to_s] != value
-      end
-
-      if changes_to_leafable.present?
-        new_leafable = leafable.dup_with_attachments
-        new_leafable.update!(leafable_params)
-        edits.revision.create!(leafable: leafable)
-        update! leaf_params.merge(leafable: new_leafable)
-      else
-        update! leaf_params
-      end
+    if will_change_leafable?(leafable_params)
+      update_leafable leaf_params, leafable_params
+    else
+      update! leaf_params
     end
   end
 
   private
+    def will_change_leafable?(leafable_params)
+      leafable_params.select do |key, value|
+        leafable.attributes[key.to_s] != value
+      end.present?
+    end
+
+    def update_leafable(leaf_params, leafable_params)
+      transaction do
+        new_leafable = leafable.dup_with_attachments
+        new_leafable.update!(leafable_params)
+        edits.revision.create!(leafable: leafable)
+        update! leaf_params.merge(leafable: new_leafable)
+      end
+    end
+
     def record_moved_to_trash
       edits.trash.create!(leafable: leafable)
     end
