76eb2c2bd54afff263efd931c2eaed1307b9e34c
Kevin McConnell <kevin@37signals.com>
2024-06-13 11:32:43 +0100

    Use form attribute && CSS for theme switching

--- app/javascript/controllers/remote_click/trigger_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static outlets = ["remote-click"]
-
-  click() {
-    this.remoteClickOutlet.click()
-  }
-}
