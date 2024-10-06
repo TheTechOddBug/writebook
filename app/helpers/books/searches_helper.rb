53354e71633450126f4cbf7273723b95b29b8ce0
Kevin McConnell <kevin@37signals.com>
2024-10-06 15:50:54 -0400

    Consolidate helpers

--- app/helpers/books/searches_helper.rb
+++ app/helpers/books/searches_helper.rb
@@ -1,2 +1,9 @@
 module Books::SearchesHelper
+  def highlighted_match(match)
+    sanitize match, tags: %w[ mark ]
+  end
+
+  def search_button(book)
+    render "books/searches/search", book: book
+  end
 end
--- app/helpers/searches_helper.rb
+++
@@ -1,9 +0,0 @@
-module SearchesHelper
-  def highlighted_match(match)
-    sanitize match, tags: %w[ mark ]
-  end
-
-  def search_button(book)
-    render "books/searches/search", book: book
-  end
-end
