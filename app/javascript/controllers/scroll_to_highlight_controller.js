79ddec461e978d7548a6e0e2fd0c7bd3d375eaa5
Kevin McConnell <kevin@37signals.com>
2024-10-23 13:57:11 +0100

    Resolve JS error on pages without search param

--- app/javascript/controllers/scroll_to_highlight_controller.js
+++ app/javascript/controllers/scroll_to_highlight_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import { nextFrame } from "helpers/timing_helpers"
 
 export default class extends Controller {
   async connect() {
-    await nextFrame()
-    this.element.getElementsByTagName("mark")[0].scrollIntoView({ behavior: "smooth", block: "center" })
+    this.element.querySelector("mark")?.scrollIntoView({ behavior: "smooth", block: "center" })
   }
 }
